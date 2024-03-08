45316f5958ae34aaa1738f0b258cd0fc7c876b72
Paulina <pauliguz@gmail.com>
2024-03-08 15:43:57 -0300

    se agraga carpeta assets

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+  $("#enviar").on("click", function () {
+    alert("El mensaje fue enviado correctamente");
+  });
+
+  $(".ingredientesTxt, .preparacionTxt").on("dblclick", function () {
+    $(this).css("color", "red");
+  });
+
+  $(".embalse").on("click", function () {
+    $("#embalse2").toggle();
+  });
+  $(".lago").on("click", function () {
+    $("#lago2").toggle();
+  });
+  $(".salar").on("click", function () {
+    $("#salar2").toggle();
+  });
+
+
+
+});
+$(".bosque").on("click", function () {
+  $("#bosque2").toggle();
+});
+$(function () {
+  $('[data-toggle="popover"]').popover();
+});
